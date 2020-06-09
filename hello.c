b75dd2cf097b8d54e281ad93db12a0836978ed32
noahhampl <66633104+noahhampl@users.noreply.github.com>
2020-06-09 09:05:15 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Chris Bourke
+ *         cbourke@cse.unl.edu
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
